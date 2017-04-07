62ede805e093261888db7f9bb127eeb50dfa8bf8
Adam Cavendish <GetbetterABC@yeah.net>
2017-04-07 15:19:39 +0800

    Add REASON_EXCLUDE_LINK_DEPENDENCY for custom exclude

--- reason.cmake
+++ reason.cmake
@@ -5,25 +5,24 @@ endif()
 # Use to specify where the 'reason.cmake' directory is
 set(REASON_MODULE_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
-# Global Variables @TODO Document it
-# 1. REASON_VERBOSE
-# 2. REASON_USE_COTIRE
-# 3. REASON_BRIEF_PATH
-
-# Reasonable CMake Project
-include("reason.color")
-include("reason.util")
 # Use cotire if possible
 # @see https://github.com/sakra/cotire
 include(cotire OPTIONAL)
 
+# Reasonable CMake Project
+include("reason.properties")
+include("reason.color")
+include("reason.util")
+
 reason_verbose("reason verbose: ON")
+
 include("reason.add_library")
 include("reason.add_interface_library")
 include("reason.add_executable")
 include("reason.install")
 include("reason.add_multiple_tests")
 include("reason.pack_deb")
+
 reason_message(STATUS "reason loaded")
 
 function(reason)
